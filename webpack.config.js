646f3636f1794792b8b22acb5a301bff1a5414d5
JellyLu <634725546@qq.com>
2017-11-15 23:55:08 +0800

    move babel config to file .babelrc

--- webpack.config.js
+++ webpack.config.js
@@ -16,9 +16,6 @@ module.exports = {
       test: /(\.jsx|\.js)$/,
       use: {
         loader: "babel-loader",
-        options: {
-          presets: ["env", "react"]
-        }
       },
       exclude: /node-modules/
     }
