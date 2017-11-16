0ca54e3c1b3d668e9b9a355c04c42b4f86027b71
JellyLu <634725546@qq.com>
2017-11-16 21:55:34 +0800

    add css modules and camelCase for css-loader

--- app/header.js
+++ app/header.js
@@ -1,10 +1,11 @@
 import React, {Component} from 'react'
 import config from './config.json';
+import styles from './header.css';
 
 class Header extends Component{
   render() {
     return (
-      <div>
+      <div className={styles.headerRoot}>
         {config.message}
       </div>
     );
