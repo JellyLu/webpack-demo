a14cefb569d1d12db97aff16dc442672d267572c
JellyLu <634725546@qq.com>
2017-11-19 17:07:57 +0800

    make the dev environment support cache

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
   entry:  __dirname + "/app/main.js",
   output: {
     path: __dirname + "/public",
-    filename: "bundle.js"
+    filename: "bundle-[hash].js"
   },
   devtool: "eval-source-map",
   devServer: {
